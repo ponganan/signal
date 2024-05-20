9d825be94f0f678c68b8ad862ab61dce854e6002
ponganan <joeyubu@gmail.com>
2024-05-20 10:29:20 +0700

    signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'signal';
+
+  //basic initialize signal
+  firstName = signal('Ponganan');
+
+  lastName = signal<string>('Saensuk');
+
+  age = signal<number>(41);
+
+  phoneNumber = signal<number>(66654195222);
+
+  // add computed signal value
+
+  fullName = computed(() => this.fName() + " " + this.mName() + " " + this.lName());
+
+  fName = signal('');
+  mName = signal('');
+  lName = signal('');
+
+  // test Observable
+  // add CommonModule for use Observable 
+  address: Observable<string> = of('44 Soi xxx');;
+
+  constructor() {
+
+    //this.demoName = of('Initial Value');
+
+  }
+
+  changeName() {
+    // change signal value
+    this.firstName.set("JOEY");
+  }
+
+  changeFName(value: string) {
+    this.fName.set(value);
+  }
+
+  changeMName(value: string) {
+    this.mName.set(value);
+  }
+
+  changeLName(value: string) {
+    this.lName.set(value);
+  }
+
 }
